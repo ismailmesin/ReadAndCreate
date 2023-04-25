13b13ea320d62edafa44f3f2d6ee0adb69a4cd5f
ismailmesin <113332232+ismailmesin@users.noreply.github.com>
2023-04-25 10:20:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReadAndCreate
+ A simple program that reads data from a XML file and extracts specific info and creates a new XML file
---
+++ ReadAndCreate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml;
+using System.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+
+namespace ReadAndCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new();
+
+            doc.Load(@"C:\Users\ismail\Desktop\Etteplan.XML\Etteplan.XML\Data.xml");
+
+
+            XmlNodeList nodeList = doc.SelectNodes("//body/trans-unit");
+
+
+            foreach (XmlNode node in nodeList)
+            {
+                string id = node.Attributes["id"].Value;
+                XmlNode unit = node.SelectSingleNode("target");
+                if (id == "42007")
+                {
+                    Console.WriteLine(unit.InnerText);
+
+                    string data = unit.InnerText;
+
+                    XmlDocument newDoc = new();
+                    newDoc.LoadXml($"<body><trans-unit>{data}</trans-unit></body>");
+                    newDoc.Save("info.xml");
+
+                }
+
+            }
+        }
+    }
+}
